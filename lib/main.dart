16130d6726fdc85c36fa4492032c1528da99e1f8
ALEXTZ69 <85032793+ALEXTZ69@users.noreply.github.com>
2024-06-21 17:19:43 -0500

    main fix  y profile fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';  
-import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
+import 'firebase_options.dart';
+import 'routes.dart';
+import 'theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,10 +18,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Food Facil',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: HomeScreen(),
+      theme: appTheme,
+      home: SplashScreen(),
+      routes: AppRoutes.getRoutes(),
     );
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,27 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'login_screen.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _signOut() async {
+    await _auth.signOut();
+    Navigator.of(context).pushReplacementNamed('/login');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Perfil del usuario'),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () async {
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => LoginScreen()),
-              );
-            },
-            child: Text('Cerrar sesión'),
-          ),
-        ],
+    User? user = _auth.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (user != null) ...[
+              Text('Correo Electrónico: ${user.email}'),
+              SizedBox(height: 20),
+            ],
+            ElevatedButton(
+              onPressed: _signOut,
+              child: Text('Cerrar Sesión'),
+            ),
+          ],
+        ),
       ),
     );
   }
